cc1604615936c7606c9da6dfc4ba884b90bada3d
Robert Carlo <carlotubig@gmail.com>
2022-12-13 18:06:51 +0800

    color bug tailwind

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const AppContainer = styled.div`
 
 function App() {
   return (
-    <div className="max-w-[1000px] color-[#ffff] mx-auto">
+    <div className="max-w-[1000px] text-white mx-auto">
       <Header />
       <Introduction />
       <Showcase />
